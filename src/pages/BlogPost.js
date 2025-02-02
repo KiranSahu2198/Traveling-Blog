9ec6814866a98eec9418ff6f0d8033ae1dcdb0e0
Kiran <kiransahu2198@gmail.com>
2025-02-02 12:00:10 +0530

    initialization

---
+++ src/pages/BlogPost.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BlogPost = () => {
+  return (
+    <div>
+      <h1>Blog Post Title</h1>
+      <p>This is a detailed blog post about my travel experiences.</p>
+    </div>
+  );
+};
+
+export default BlogPost;
